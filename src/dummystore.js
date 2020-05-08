52db63e090eb6c92ecf8713e5a4aa2c3972c4c08
Brenden Dowd <brenden.dowd@yahoo.com>
2020-05-07 19:59:13 -0700

    updated landingpage and added dummystore for static client

---
+++ src/dummystore.js
@@ -0,0 +1,10 @@
+export default {
+  "users":[{
+    "id":"1",
+    "name":"John Doe",
+    "email":"jd@gmail.com",
+    "location":"?"
+  }],
+  "stories":[],
+  "comments":[],
+}
---
+++ src/routes/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function LandPage() {
+  return (
+    <section>
+      <h2>It Takes A village</h2>
+      logo
+      <p>It Takes a Village is a community based altruism application that lets people come together to help the people around them. If you are in need of assistance or you are fortunate enough to be in a position to give back, you can connect to the people in your community.
+      </p>
+      {/* buttons */}
+    </section>
+  )
+}
