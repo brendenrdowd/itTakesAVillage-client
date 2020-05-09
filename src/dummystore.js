b6c1b7f9df615989d0c617216de7451cd51ab386
anotherchester <anotherchester@gmail.com>
2020-05-09 13:24:24 -0700

    dummy upd

--- src/dummystore.js
+++ src/dummystore.js
@@ -1,69 +1,72 @@
-//what should I pass into location? X, Y, Z coordinates or state code and the city?
+//zipcode
 
 export default {
   "users":[{
     "id":"1",
     "name":"John Doe",
     "email":"jd@gmail.com",
-    "location":"?"
+    "location":"90027"
   },
   {
     "id":"2",
     "name":"Commander Spock",
     "email":"spock@enterprise.space",
-    "location":"?"
+    "location":"90013"
   },
   {
     "id":"3",
     "name":"Darth Revan",
     "email":"sithlord@deathstar.com",
-    "location":"?"
+    "location":"90013"
   },
   {
     "id":"4",
     "name":"Dwayne Johnson",
     "email":"therock@wwe.com",
-    "location":"?"
+    "location":"90048"
   },
   {
     "id":"5",
     "name":"X Æ A-12",
     "email":"thanksdad@spacex.com",
-    "location":"?"
+    "location":"90291"
   }],
   "stories":[{
-    "story_id" : "01",
+    "id" : "01",
     "user_id" : "4",
     "title" : "Looking for a help with a groceries",
     "issue" : "Looking for a person, who can go to WholeFoods and buy a groceries from a list for me and arrange a non-contact delivery to my house",
-    "keywords" : "help, groceries, delivery"
+    "keywords" : "help, groceries, delivery",
+    "result" : "true"
   },
   {
-    "story_id" : "02",
+    "id" : "02",
     "user_id" : "1",
     "title" : "Food offer",
     "issue" : "I have a lot of plums and can deliver them to any address in a Bay Area",
-    "keywords" : "food offer, delivery"
+    "keywords" : "food offer, delivery",
+    "result" : "true"
   },
   {
-    "story_id" : "03",
+    "id" : "03",
     "user_id" : "5",
     "title" : "Rideshare",
     "issue" : "Will drive you anywhere you need, can help with moving also (I have a big Tesla Truck)",
-    "keywords" : "rideshare, transportation, moving"
+    "keywords" : "rideshare, transportation, moving",
+    "result" : "true"
   }],
   "comments":[{
-    "story_id" : "03",
+    "id" : "03",
     "user_id" : "2",
-    "content" : "Can you give me a ride to Phoenix?"
+    "content" : "Can you give me a ride to Phoenix?",
   },
   {
-    "story_id" : "02",
+    "id" : "02",
     "user_id" : "4",
     "content" : "That stuff is awesome!"
   },
   {
-    "story_id" : "01",
+    "id" : "01",
     "user_id" : "3",
     "content" : "Join the Dark side first and I'll do that for you!"
   }],
--- src/routes/StoryPage/StoryPage.js
+++ src/routes/StoryPage/StoryPage.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import CreateCommentForm from '../../components/CreateCommentForm/CreateCommentForm'
+import StoryCard from '../../components/StoryCard/StoryCard'
 
 export class StoryPage extends Component {
   render() {
     return (
       <section>
-        {/* card itself */}
+        {StoryCard}
         {CreateCommentForm}
         {/* comments */}
         {/* resolution? */}
