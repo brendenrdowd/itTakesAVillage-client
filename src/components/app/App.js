7905c510f2e0d7fd1ccc44316c9b54450ab79894
Fernando Gaol <ffilho@riolindo.org>
2020-05-09 12:14:24 -0600

    no changes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,54 +1,45 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
-import ApiContext from '../../contexts/ApiContext'
-import PrivateRoute from '../Utils/PrivateRoute'
-import PublicOnlyRoute from '../Utils/PublicOnlyRoute'
+import { Route, Switch } from 'react-router-dom';
+import ApiContext from '../../contexts/ApiContext';
+import PrivateRoute from '../Utils/PrivateRoute';
+import PublicOnlyRoute from '../Utils/PublicOnlyRoute';
 // import all the routes
-import DashboardPage from '../../routes/DashboardPage/DashboardPage'
-import LandingPage from '../../routes/LandingPage/LandingPage'
-import LoginPage from '../../routes/LoginPage/LoginPage'
-import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage'
-import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage'
-import './App.css'
+import DashboardPage from '../../routes/Dashboard/Dashboard';
+import LandingPage from '../../routes/LandingPage/LandPage';
+import LoginPage from '../../routes/LoginPage/LoginPage';
+import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage';
+import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage';
+import Nav from '../../components/Nav/Nav';
+import './App.css';
 
 class App extends Component {
-  state = {}
+  state = {};
 
   render() {
-    const value = {}
+    // const value = {};
     return (
-      <ApiContext.Provider value={value}>
-        <div className="App container">
+      // <ApiContext.Provider value={value}>
+        <div className='App container'>
+          <header>
+            <Nav />
+          </header>
           <main>
-            {this.state.hasError && <p className='red'>{this.state.error}</p>}
+            {/* {this.state.hasError && <p className='red'>{this.state.error}</p>} */}
             <Switch>
-              <PublicOnlyRoute
-                exact
-                path={'/'}
-                component={LandingPage}
-              />
-              <PublicOnlyRoute
-                path={'/login'}
-                component={LoginPage}
-              />
+              <PublicOnlyRoute exact path={'/'} component={LandingPage} />
+              <PublicOnlyRoute path={'/login'} component={LoginPage} />
               <PublicOnlyRoute
                 path={'/register'}
                 component={RegistrationPage}
               />
-              <PrivateRoute
-                path={'/dashboard'}
-                component={DashboardPage}
-              />
-              <Route
-                component={NotFoundPage}
-              />
+              <PrivateRoute path={'/dashboard'} component={DashboardPage} />
+              <Route component={NotFoundPage} />
             </Switch>
           </main>
         </div>
-      </ApiContext.Provider>
-    )
+      // </ApiContext.Provider>
+    );
   }
 }
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom'
-import history from './history'
+import { Router } from 'react-router-dom';
+import history from './history';
 import App from './components/App/App';
 // import { library } from '@fortawesome/fontawesome-svg-core'
 // import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -14,6 +14,5 @@ ReactDOM.render(
   <Router history={history}>
     <App />
   </Router>,
-  document.getElementById('root'));
-
-
+  document.getElementById('root')
+);
