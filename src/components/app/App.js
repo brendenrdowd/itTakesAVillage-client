04467aa53630992ddaa5928038e4d60aa2d71316
tomilone <tonymilone.tech@gmail.com>
2020-05-12 21:48:07 -0500

    added context

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import ApiContext from "../../contexts/ApiContext";
 import PrivateRoute from "../Utils/PrivateRoute";
 import PublicOnlyRoute from "../Utils/PublicOnlyRoute";
-import Nav from "../../components/Nav/Nav";
-// import ApiContext from "../../contexts/ApiContext";
 // import all the routes
 import DashboardPage from "../../routes/DashboardPage/DashboardPage";
 import LandingPage from "../../routes/LandingPage/LandingPage";
@@ -24,34 +23,37 @@ class App extends Component {
 
   render() {
     // what is our context going to look like?
-    // const value = {};
+    const value = {};
     return (
-      // <ApiContext.Provider value={value}>
-      <div className="App container">
-        <main>
-          {this.state.hasError && <p className="red">{this.state.error}</p>}
-          <Switch>
-            <PublicOnlyRoute exact path={"/"} component={LandingPage} />
-            <PublicOnlyRoute path={"/login"} component={LoginPage} />
-            <PublicOnlyRoute path={"/register"} component={RegistrationPage} />
-            {/* private */}
-            <Route path={"/dashboard"} component={DashboardPage} />
-            {/* Private */}
-            <Route path={"/create"} component={CreateStoryPage} />
-            {/* private route */}
-            {/* we need to load the commentComponent instead of the createStory component, probably by indicating with props... */}
-            {/* <Route
+      <ApiContext.Provider value={value}>
+        <div className="App container">
+          <main>
+            {this.state.hasError && <p className="red">{this.state.error}</p>}
+            <Switch>
+              <PublicOnlyRoute exact path={"/"} component={LandingPage} />
+              <PublicOnlyRoute path={"/login"} component={LoginPage} />
+              <PublicOnlyRoute
+                path={"/register"}
+                component={RegistrationPage}
+              />
+              {/* private */}
+              <Route path={"/dashboard"} component={DashboardPage} />
+              {/* Private */}
+              <Route path={"/create"} component={CreateStoryPage} />
+              {/* private route */}
+              {/* we need to load the commentComponent instead of the createStory component, probably by indicating with props... */}
+              {/* <Route
                 path={'/comment/edit/:id'}
                 component={CreateStoryPage} /> */}
-            <Route path={"/story/:id"} component={StoryPage} />
-            {/* private route */}
-            <Route path={"/story/edit/:id"} component={CreateStoryPage} />
-            <Route path={"/policies"} component={PoliciesPage} />
-            <Route component={NotFoundPage} />
-          </Switch>
-        </main>
-      </div>
-      // </ApiContext.Provider>
+              <Route path={"/story/:id"} component={StoryPage} />
+              {/* private route */}
+              <Route path={"/story/edit/:id"} component={CreateStoryPage} />
+              <Route path={"/policies"} component={PoliciesPage} />
+              <Route component={NotFoundPage} />
+            </Switch>
+          </main>
+        </div>
+      </ApiContext.Provider>
     );
   }
 }
