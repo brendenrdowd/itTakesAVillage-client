fd6c217ddf0f21c0d4e86f4f7cf86dc421bdfe9e
Rupali <rupalirajput@gmail.com>
2020-05-20 09:55:17 -0500

    added validation to indvidualregform

--- src/components/IndividualRegForm/IndividualRegForm.js
+++ src/components/IndividualRegForm/IndividualRegForm.js
@@ -6,89 +6,89 @@ import UserApiService from '../../services/user-api-service';
 import './IndividualRegForm.css';
 import userContext from '../../contexts/ApiContext';
 
-// const initialState = {
-//   name: '',
-//   username: '',
-//   email: '',
-//   location: '',
-//   password: '',
-//   repeatPassword: '',
-//   nameError: '',
-//   userNameError: '',
-//   emailError: '',
-//   zipCodeError: '',
-//   passwordError: '',
-//   repeatPasswordError: '',
-// };
+const initialState = {
+  name: '',
+  username: '',
+  email: '',
+  location: '',
+  password: '',
+  repeatPassword: '',
+  nameError: '',
+  userNameError: '',
+  emailError: '',
+  zipCodeError: '',
+  passwordError: '',
+  repeatPasswordError: '',
+};
 export default class IndividualRegForm extends Component {
   static defaultProps = {
-    onRegistrationSuccess: () => {},
+    onRegistrationSuccess: () => { },
   };
 
-  // validation = () => {
-  //   let nameError = '';
-  //   let userNameError = '';
-  //   let emailError = '';
-  //   let zipCodeError = '';
-  //   let passwordError = '';
-  //   let repeatPasswordError = '';
+  validation = (target) => {
+    let nameError = '';
+    let userNameError = '';
+    let emailError = '';
+    let zipCodeError = '';
+    let passwordError = '';
+    let repeatPasswordError = '';
 
-  //   if (!this.state.name) {
-  //     nameError = 'Name field is empty';
-  //   }
-  //   if (!this.state.username) {
-  //     userNameError = 'Username field is empty';
-  //   }
-  //   if (!this.state.email) {
-  //     emailError = 'Email field is empty';
-  //   }
-  //   if (!validator.isEmail(this.state.email)) {
-  //     emailError = 'invalid email';
-  //   }
-  //   // if (!this.state.email.includes('@')) {
-  //   //   emailError = 'Invalid email';
-  //   // }
-  //   if (!this.state.location) {
-  //     zipCodeError = 'Zip code field is empty';
-  //   }
-  //   if (!isValidZipcode(this.state.location)) {
-  //     zipCodeError = 'invalid zip code';
-  //   }
-  //   if (!Number(this.state.location)) {
-  //     zipCodeError = 'must be numbers';
-  //   }
-  //   if (this.state.location.length !== 5) {
-  //     zipCodeError = 'must be 5 characters long';
-  //   }
-  //   if (!this.state.password) {
-  //     passwordError = 'Password field is empty';
-  //   }
-  //   if (!this.state.repeatPassword) {
-  //     repeatPasswordError = 'Password field is empty';
-  //   }
-  //   if (this.state.repeatPassword.length !== this.state.password.length) {
-  //     repeatPasswordError = 'Passwords must match';
-  //   }
-  //   if (
-  //     nameError ||
-  //     userNameError ||
-  //     emailError ||
-  //     zipCodeError ||
-  //     passwordError ||
-  //     repeatPasswordError
-  //   ) {
-  //     this.setState({
-  //       nameError,
-  //       userNameError,
-  //       emailError,
-  //       zipCodeError,
-  //       passwordError,
-  //       repeatPasswordError,
-  //     });
-  //     return false;
-  //   }
-  //   return true;
-  // };
+    if (!target.name.value) {
+      nameError = 'Name field is empty';
+    }
+    if (!target.username.value) {
+      userNameError = 'Username field is empty';
+    }
+    if (!target.email.value) {
+      emailError = 'Email field is empty';
+    }
+    if (!validator.isEmail(target.email.value)) {
+      emailError = 'invalid email';
+    }
+    if (!target.email.value.includes('@')) {
+      emailError = 'Invalid email';
+    }
+    if (!target.location.value) {
+      zipCodeError = 'Zip code field is empty';
+    }
+    if (!isValidZipcode(target.location.value)) {
+      zipCodeError = 'invalid zip code';
+    }
+    if (!Number(target.location.value)) {
+      zipCodeError = 'must be numbers';
+    }
+    if (target.location.value.length !== 5) {
+      zipCodeError = 'must be 5 characters long';
+    }
+    if (!target.password.value) {
+      passwordError = 'Password field is empty';
+    }
+    if (!target.repeatPassword.value.length) {
+      repeatPasswordError = 'Password field is empty';
+    }
+    if (target.repeatPassword.value.length !== target.password.value.length) {
+      repeatPasswordError = 'Passwords must match';
+    }
+    this.setState({
+      nameError,
+      userNameError,
+      emailError,
+      zipCodeError,
+      passwordError,
+      repeatPasswordError,
+    });
+    if (
+      nameError ||
+      userNameError ||
+      emailError ||
+      zipCodeError ||
+      passwordError ||
+      repeatPasswordError
+    ) {
+      return false;
+    }
+    return true;
+  };
   state = { error: null }; // error and logic will come from back end
 
   handleSubmit = (ev) => {
@@ -102,31 +102,34 @@ export default class IndividualRegForm extends Component {
       repeatPassword,
     } = ev.target;
 
-    this.setState({ error: null });
-   
-      UserApiService.postUser({
-        name: name.value,
-        username: username.value,
-        email: email.value,
-        location: location.value,
-        password: password.value,
-        repeatPassword: repeatPassword.value,
-      })
-
-        .then((user) => {
-          name.value = '';
-          username.value = '';
-          email.value = '';
-          location.value = '';
-          password.value = '';
-          repeatPassword.value = '';
-          this.props.onRegistrationSuccess();
-        })
-        .catch((res) => {
-          this.setState({ error: res.error });
-        });
+    if (this.validation(ev.target)) {
+      return;
     }
-  };
+    this.setState({ error: null });
+
+    UserApiService.postUser({
+      name: name.value,
+      username: username.value,
+      email: email.value,
+      location: location.value,
+      password: password.value,
+      repeatPassword: repeatPassword.value,
+    })
+
+      .then((user) => {
+        name.value = '';
+        username.value = '';
+        email.value = '';
+        location.value = '';
+        password.value = '';
+        repeatPassword.value = '';
+        this.props.onRegistrationSuccess();
+      })
+      .catch((res) => {
+        this.setState({ error: res.error });
+      });
+  }
+
   render() {
     const { error } = this.state;
     return (
@@ -144,6 +147,7 @@ export default class IndividualRegForm extends Component {
             // required
             id='IndividualRegForm__name'
           ></Input>
+          {this.state.nameError}
         </div>
 
         <div className='username'>
@@ -156,12 +160,14 @@ export default class IndividualRegForm extends Component {
             type='text'
             id='IndividualRegForm__username'
           ></Input>
+          {this.state.userNameError}
         </div>
         <div className='email'>
           <label htmlFor='IndividualRegForm__email'>
             Email <Required />
           </label>
           <Input name='email' type='text' id='IndividualRegForm__email'></Input>
+          {this.state.emailError}
         </div>
         <div className='location'>
           <label htmlFor='IndividualRegForm__location'>
@@ -173,6 +179,7 @@ export default class IndividualRegForm extends Component {
             type='text'
             id='IndividualRegForm__location'
           ></Input>
+          {this.state.zipCodeError}
         </div>
         <div className='password'>
           <label htmlFor='IndividualRegForm__password'>
@@ -184,6 +191,7 @@ export default class IndividualRegForm extends Component {
             type='password'
             id='IndividualRegForm__password'
           ></Input>
+          {this.state.passwordError}
         </div>
 
         <div className='re-enter-password'>
@@ -196,6 +204,7 @@ export default class IndividualRegForm extends Component {
             type='password'
             id='IndividualRegForm__password'
           ></Input>
+          {this.state.repeatPasswordError}
         </div>
         <Button type='submit'>Sign up</Button>
       </form>
