63d20d81b26c5425d0d15e5a0ef4ddf8b493287d
Humberto Hernandez <hamburger45@gmail.com>
2020-05-19 19:00:19 -0700

    added smoke test

--- src/components/CreateStoryForm/CreateStoryForm.test.js
+++ src/components/CreateStoryForm/CreateStoryForm.test.js
@@ -1,16 +1,6 @@
-// import React from "react";
-// import CreateStoryForm from "./CreateStoryForm";
-
-// import { shallow } from "enzyme";
-
-// it("renders without crashing", () => {
-//   shallow(<CreateStoryForm />);
-// });
-
 import React from "react";
 import ReactDOM from "react-dom";
 import CreateStoryForm from "./CreateStoryForm";
-// import DrawerToggleButton from './DrawerToggleButton';
 import { BrowserRouter } from "react-router-dom";
 
 describe("CreateStoryForm Component", () => {
@@ -26,10 +16,3 @@ describe("CreateStoryForm Component", () => {
     ReactDOM.unmountComponentAtNode(div);
   });
 });
-// describe('Drawer Toggle Component', () => {
-//   it('renders without crashing', () => {
-//     const div = document.createElement('div');
-//     ReactDOM.render(<BrowserRouter> <DrawerToggleButton /> </BrowserRouter>, div);
-//     ReactDOM.unmountComponentAtNode(div);
-//   })
-// })
