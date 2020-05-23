415ce4c714d36f965bcb3693f4d5032f2d085eb3
anotherchester <anotherchester@gmail.com>
2020-05-22 20:52:02 -0700

    comment form test passed

---
+++ src/components/CreateCommentForm/CreateCommentForm.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CreateCommentForm from "./CreateCommentForm";
+import { BrowserRouter } from "react-router-dom";
+
+describe("CreateCommentForm Component", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+      <BrowserRouter>
+        {" "}
+        <CreateCommentForm />{" "}
+      </BrowserRouter>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
