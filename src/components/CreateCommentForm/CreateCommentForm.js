a2e24dc6dca9ce5ec27c22b32e4bdda5083a538e
Brenden Dowd <brenden.dowd@yahoo.com>
2020-06-09 17:40:09 -0700

    fixed commentform bug with userid

--- src/components/CreateCommentForm/CreateCommentForm.js
+++ src/components/CreateCommentForm/CreateCommentForm.js
@@ -33,7 +33,7 @@ class CreateCommentForm extends Component {
     event.preventDefault();
 
     const { comment } = event.target;
-    const  userId  = localStorage.getItem("user_id");
+    const  userId  = Number(localStorage.getItem("user_id"));
 
     this.setState({ error: null });
 
